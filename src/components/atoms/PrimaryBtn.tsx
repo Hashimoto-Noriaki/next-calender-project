4e9f35c7561b53dc99e9f5cc3ee7e1e3b29b82ea
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-25 23:56:51 +0900

    Topページ作成のリファクタリング

---
+++ src/components/atoms/PrimaryBtn.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+type PropsType = {
+    children: ReactNode
+}
+
+export const PrimaryBtn = ({ children }: PropsType) => {
+
+    return (
+        <button className="bg-lime-800 text-white p-4 text-lg rounded-lg">
+            { children }
+        </button>
+    )
+}
