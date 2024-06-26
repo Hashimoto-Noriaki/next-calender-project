6fdade78ea54129fdb0e1b1d8ff77307462853b5
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-26 23:00:20 +0900

    インデント修正

--- src/components/templates/NotLoginLayout.tsx
+++ src/components/templates/NotLoginLayout.tsx
@@ -12,8 +12,8 @@ export const NotLoginLayout = ({ children }: PropsType) => {
           <p className="logo">スケジュール管理APP</p>
           <nav>
             <ul className="flex gap-5 text-lime-800">
-            <li>ご利用方法</li>
-            <li>ログイン</li>
+              <li>ご利用方法</li>
+              <li>ログイン</li>
             </ul>
           </nav>
         </div>
